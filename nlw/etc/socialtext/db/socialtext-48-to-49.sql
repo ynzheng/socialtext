c89ca8296832b25840947aa5e1c275d6c64d44a1
Kevin Jones <kevin.jones@socialtext.com>
2009-04-02 13:43:29 -0700

    Update gitignore and modify the migration

--- nlw/etc/socialtext/db/socialtext-48-to-49.sql
+++ nlw/etc/socialtext/db/socialtext-48-to-49.sql
@@ -7,8 +7,6 @@ UPDATE gadget
   SET name = regexp_replace(src, '^.*?([^/]*).xml$', '\\1')
 WHERE plugin IS NOT NULL;
 
-SELECT plugin, name, src from gadget;
-
 UPDATE "System"
    SET value = '49'
  WHERE field = 'socialtext-schema-version';
