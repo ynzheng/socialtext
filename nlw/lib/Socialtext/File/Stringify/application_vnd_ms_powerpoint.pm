f9d50c9c639acfe8d21aad698100321e7c8ce786
swdevlin <shawn.devlin@socialtext.com>
2009-04-07 09:17:54 -0700

    Removed unnecessary filter

--- nlw/lib/Socialtext/File/Stringify/application_vnd_ms_powerpoint.pm
+++ nlw/lib/Socialtext/File/Stringify/application_vnd_ms_powerpoint.pm
@@ -12,10 +12,7 @@ sub to_string {
     if ( $? or $@ ) {
         $text = Socialtext::File::Stringify::Default->to_string($file);
     }
-    elsif ( defined $text ) {
-        $text =~ s/^"|"$//mg;
-        $text =~ s/"+/"/g;
-    }
+    
     return $text;
 }
 
