137b3e252fac7b0379892531a2386941bc86977c
Luke Closs <gravatar@5thplane.com>
2009-04-06 18:05:44 -0700

    remove commented out code

--- nlw/lib/Socialtext/TheSchwartz.pm
+++ nlw/lib/Socialtext/TheSchwartz.pm
@@ -7,7 +7,6 @@ use namespace::clean -except => 'meta';
 extends qw/TheSchwartz::Moosified/;
 
 has '+verbose' => ( default => ($ENV{ST_JOBS_VERBOSE} ? 1 : 0) );
-#has '+verbose' => ( default => 1 );
 
 # make sure to call get_dbh() every time, basically
 override 'databases' => sub { return [ get_dbh() ] };
