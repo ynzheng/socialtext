7b2b75d853e392cb3617604982d1a5f8706944e4
Stash <jeremy.stashewsky@socialtext.com>
2009-04-08 10:35:19 -0700

    Refactor Check_and_drop_privs into DaemonUtil.

--- nlw/lib/Socialtext/CLI.pm
+++ nlw/lib/Socialtext/CLI.pm
@@ -27,6 +27,7 @@ use Socialtext::User::Cache;
 use Socialtext::Timer;
 use Socialtext::SystemSettings qw/get_system_setting set_system_setting/;
 use Socialtext::Pluggable::Adapter;
+use Socialtext::DaemonUtil;
 use Fatal qw/mkdir rmtree/;
 
 my %CommandAliases = (
@@ -86,53 +87,12 @@ sub run {
     }
 
     if ( $command ne 'help' ) {
-        Check_and_drop_privs();
+        Socialtext::DaemonUtil::Check_and_drop_privs();
     }
 
     Socialtext::Timer->Continue("CLI_$command");
     $self->$command();
     Socialtext::Timer->Pause("CLI_$command");
-}
-
-sub Check_and_drop_privs {
-    if ($>) {
-        _check_privs();
-    }
-    else {
-        _drop_privs();
-    }
-}
-
-sub _check_privs {
-    my $self = shift;
-
-    my $data_root = Socialtext::AppConfig->data_root_dir();
-    return if -w $data_root;
-
-    my $uid      = ( stat $data_root )[4];
-    my $username = getpwuid $uid;
-
-    warn <<"EOF";
-
- The data root dir at $data_root is not writeable by this process.
-
- It is owned by $username.
-
- You can either run this process as $username or root in order to run
- this command.
-
-EOF
-
-    _exit(1);
-}
-
-sub _drop_privs {
-    my ( $uid, $gid )
-        = ( stat Socialtext::AppConfig->data_root_dir() )[ 4, 5 ];
-
-    require POSIX;
-    POSIX::setgid($gid);
-    POSIX::setuid($uid);
 }
 
 sub help {
---
+++ nlw/lib/Socialtext/DaemonUtil.pm
@@ -0,0 +1,46 @@
+package Socialtext::DaemonUtil;
+use warnings;
+use strict;
+
+sub Check_and_drop_privs {
+    if ($>) {
+        _check_privs();
+    }
+    else {
+        _drop_privs();
+    }
+}
+
+sub _check_privs {
+    my $self = shift;
+
+    my $data_root = Socialtext::AppConfig->data_root_dir();
+    return if -w $data_root;
+
+    my $uid      = ( stat $data_root )[4];
+    my $username = getpwuid $uid;
+
+    warn <<"EOF";
+
+ The data root dir at $data_root is not writeable by this process.
+
+ It is owned by $username.
+
+ You can either run this process as $username or root in order to run
+ this command.
+
+EOF
+
+    _exit(1);
+}
+
+sub _drop_privs {
+    my ( $uid, $gid )
+        = ( stat Socialtext::AppConfig->data_root_dir() )[ 4, 5 ];
+
+    require POSIX;
+    POSIX::setgid($gid);
+    POSIX::setuid($uid);
+}
+
+1;
