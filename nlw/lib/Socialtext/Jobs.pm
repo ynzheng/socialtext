1a87f81c915346b3cf34b2196c076b095173244a
Stash <jeremy.stashewsky@socialtext.com>
2009-04-13 16:25:21 -0700

    refactor job stringification

--- nlw/lib/Socialtext/Jobs.pm
+++ nlw/lib/Socialtext/Jobs.pm
@@ -7,6 +7,7 @@ use Module::Pluggable search_path => 'Socialtext::Job',
     sub_name => 'job_types', require => 0;
 use Memoize;
 use Carp qw/croak/;
+use POSIX qw/strftime/;
 use namespace::clean -except => [qw(meta job_types)];
 
 has '_client' => (
@@ -38,6 +39,49 @@ sub can_do_all {
 }
 
 sub clear_jobs { sql_execute('DELETE FROM job') }
+
+sub job_to_string {
+    my $class = shift;
+    my $job = shift;
+    my $opts = shift || {};
+
+    my $string = strftime "%Y-%m-%d.%H:%M:%S", localtime($job->insert_time);
+
+    $string .= " id=" . $job->jobid;
+    (my $shortname = $job->funcname) =~ s/^Socialtext::Job:://;
+    $string .= ";type=$shortname";
+
+    return $string unless ref($job->arg) eq 'HASH';
+
+    if (my $ws_id = $job->arg->{workspace_id}) {
+        $opts->{ws_names} ||= {};
+        eval {
+            $opts->{ws_names}{$ws_id} ||=
+                Socialtext::Workspace->new( workspace_id => $ws_id )->name;
+            $string .= ";ws=$opts->{ws_names}{$ws_id}";
+        };
+        if ($@) {
+            $string .= ";ws=$ws_id";
+        }
+    }
+    $string .= ";page=".$job->arg->{page_id}
+        if $job->arg->{page_id};
+
+    if ($shortname eq 'Cmd') {
+        $string .= ";cmd=".$job->arg->{cmd};
+        $string .= ";args=".join(',',@{$job->arg->{args} || []});
+    }
+
+    if ($job->run_after > time) {
+        my $when = strftime("%Y-%m-%d.%H:%M:%S", localtime($job->run_after));
+        $string .= " (delayed until $when)";
+    }
+
+    $string .= " (*)" if $job->grabbed_until;
+
+    return $string unless wantarray;
+    return ($string, $shortname);
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
