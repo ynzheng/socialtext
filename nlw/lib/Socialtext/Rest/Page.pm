406fbb24883e365ead9bca880e23ef5d6a3c4b20
Brandon Noard <brandon.noard@socialtext.com>
2009-04-22 07:47:29 -0700

    Prevent unitialized value warning.

--- nlw/lib/Socialtext/Rest/Page.pm
+++ nlw/lib/Socialtext/Rest/Page.pm
@@ -95,7 +95,7 @@ sub _link_dictionary {
     $link_dictionary = undef
         if lc($link_dictionary) eq lc($S2_LINK_DICTIONARY);
 
-    if ($link_dictionary eq $DEFAULT_LINK_DICTIONARY) {
+    if ($link_dictionary && $link_dictionary eq $DEFAULT_LINK_DICTIONARY) {
         my $url_prefix = $self->hub->current_workspace->uri;
         $url_prefix =~ s{/[^/]+/?$}{};
         $self->hub->viewer->url_prefix($url_prefix);
