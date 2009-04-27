8e6c7eb35a0ac0a27b8e5121b218d7cdb7f4ffc0
Brandon Noard <brandon.noard@socialtext.com>
2009-04-27 15:13:42 -0700

    Create a mailto link if we don't get a proper user link.

--- nlw/lib/Socialtext/Page.pm
+++ nlw/lib/Socialtext/Page.pm
@@ -1926,11 +1926,13 @@ sub edit_in_progress {
         for my $evt (@edits) {
             my $user = Socialtext::User->new(user_id => $evt->{actor}{id});
             return {
+                username => $user->best_full_name,
+                email_address => $user->email_address,
                 user_business_card => $self->hub->pluggable->hook(
                     'template.user_business_card.content', $user->user_id),
                 user_link => $self->hub->pluggable->hook(
                     'template.open_user_link.content', $user->user_id
-                ) || $user->best_full_name,
+                ),
                 minutes_ago   => int((time - str2time($evt->{at})) / 60 ),
             };
         }
