ynzheng/socialtext

commit bc2a99c5d51680880c6f03c7d9567e1b042486db

Only look for wafl blocks at start of the string
The parser checks for each of the 12 different kinds of top level
blocks.  Previously, this code would try to match all of the top level
blocks, and then use the one that matched closest to the start of the
string.

This patch locks these top level block patterns to only match the start
of the string, and adds a new block 'else' as a last case.  This last
block matches any single line (blocks are always a full line).

This greatly speeds up pattern matching, b/c matching start of string is
very fast.

nlw/lib/Socialtext/Formatter.pm
@@ -21,7 +21,7 @@ const all_phrases => [
         b_mail file tt strong em del)
 ];
 const all_blocks =>
-    [qw(wafl_block hr hx wafl_p ul ol indent table p empty_p)];
+    [qw(wafl_block hr hx wafl_p ul ol indent table p empty_p else)];
 
 field page_id         => '';
 field url_prefix      => '';

nlw/lib/Socialtext/Formatter/Block.pm
@@ -21,7 +21,7 @@ field 'level';
 sub match {
     my $self = shift;
     my $text = shift;
-    return unless $text =~ /^(\^{1,6}) +(.*?)(\s+=+)?\s*?\n+/m;
+    return unless $text =~ /\A(\^{1,6}) +(.*?)(\s+=+)?\s*?\n+/m;
     $self->level( length($1) );
     $self->set_match($2);
 }
@@ -48,7 +48,7 @@ const formatter_id  => 'p';
 const html_start    => "<p>\n";
 const html_end      => "</p>\n";
 const pattern_block =>
-    qr/(            # Capture whole thing
+    qr/\A(            # Capture whole thing
         (?:
         ^(?!        # All consecutive lines *not* starting with
         (?:
@@ -85,7 +85,7 @@ use base 'Socialtext::Formatter::Paragraph';
 use Class::Field qw( const );
 
 const formatter_id  => 'empty_p';
-const pattern_block => qr/((^\s*\n)+)/m;
+const pattern_block => qr/\A((^\s*\n)+)/m;
 const html_start    => "";
 const html_end      => "<br />";
 
@@ -95,7 +95,7 @@ use base 'Socialtext::Formatter::Paragraph';
 use Class::Field qw( const );
 
 const formatter_id  => 'wafl_p';
-const pattern_block => qr/^(\{.*\})[\ \t]*\n/m;
+const pattern_block => qr/\A(\{.*\})[\ \t]*\n/m;
 const html_start    => "";
 const html_end      => "<br />";
 
@@ -113,7 +113,7 @@ field 'start_level';
 sub match {
     my $self = shift;
     my $text = shift;
-    return unless $text =~ /^(>+)/m;
+    return unless $text =~ /\A(>+)/m;
     my $l = length($1);
     $self->level($l);
     return unless $text =~ /(^(>{$l}(?!>).*\n)+)/m;

nlw/lib/Socialtext/Formatter/Parser.pm
@@ -68,6 +68,7 @@ sub _parse_blocks {
     $unit->text(undef);
     my $units    = $unit->units;
     my $table    = $self->table;
+
     my $contains = $unit->contains_blocks;
     while ( defined $text and length $text ) {
         my $match = $self->_match_format_id( $contains, $table, $text );

nlw/lib/Socialtext/Formatter/Unit.pm
@@ -49,7 +49,9 @@ field -weak      => 'prev_unit';
 sub match {
     my $self = shift;
     my $text = shift;
-    return unless $text =~ $self->pattern_block;
+    my $orig_text = $text;
+    my $match = $text =~ $self->pattern_block;
+    return unless $match;
     $self->set_match;
 }
 
@@ -156,8 +158,10 @@ sub match {
     my $self = shift;
     my $text = shift;
     my $bullet = $self->bullet;
-    return
-        unless $text =~ /((?:^($bullet).*\n)(?:^\2(?!$bullet).*\n)*)(?:\s*\n)?/m;
+    my $orig_text = $text;
+    my $regex = qr/\A((?:^($bullet).*\n)(?:^\2(?!$bullet).*\n)*)(?:\s*\n)?/m;
+    my $match = $text =~ $regex;
+    return unless $match;
     $self->set_match;
     $self->level( length($2) );
     return 1;
@@ -263,6 +267,17 @@ const html_end_tag   => '</ol>';
 const bullet         => '\#+(?=\ +|$)';
 
 ################################################################################
+package Socialtext::Formatter::Else;
+
+use base 'Socialtext::Formatter::Container';
+use Class::Field qw( const );
+
+const formatter_id   => 'else';
+const html_start_tag => '<p>';
+const html_end_tag   => '</p>';
+const pattern_block  => qr/^(.+)\n/;
+
+################################################################################
 package Socialtext::Formatter::Table;
 
 use base 'Socialtext::Formatter::Container';
@@ -270,7 +285,7 @@ use Class::Field qw( const );
 
 const formatter_id    => 'table';
 const contains_blocks => [qw(tr)];
-const pattern_block   => qr/(((^\|.*\| \n(?=\|))|(^\|.*\|  +\n)|(?s:^\|.*?\|\n))+)/m;
+const pattern_block   => qr/\A(((^\|.*\| \n(?=\|))|(^\|.*\|  +\n)|(?s:^\|.*?\|\n))+)/m;
 
 const html_end => "</table>\n";
 

nlw/lib/Socialtext/Formatter/WaflBlock.pm
@@ -34,7 +34,7 @@ sub match {
     my $text = shift;
     return
         unless $text
-        =~ /(?:^\.([\w\-]+)\ *\n)((?:.*\n)*?)(?:^\.\1\ *\n|\z)/m;
+        =~ /\A(?:^\.([\w\-]+)\ *\n)((?:.*\n)*?)(?:^\.\1\ *\n|\z)/m;
     $self->set_match($2);
     my $method = lc $1;
     $method =~ s/-/_/g;

commit 77900c98aacd0f565546612083dace43c2aa4704

* Move the {bz: 2476} test from wikiwyg/ to s3/ as it only exhibits there.   (also we now know it's caused by bc2a99c.)

nlw/share/skin/js-test/s3/t/bz-2476.t.js
@@ -1,0 +1,31 @@
+var t = new Test.Visual();
+
+t.plan(1);
+
+var page = t.gensym();
+
+t.runAsync([
+    function() {
+        t.put_page({
+            workspace: 'admin',
+            page_name: page,
+            content: "^Header\n\none\ntwo\nthree\nfour\n",
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe(
+            "/admin/?" + page,
+            t.nextStep()
+        );
+    },
+
+    function() { 
+        t.ok(
+            (t.$('div.wiki br').length > 1), "Linebreaks should be preserved"
+        );
+
+        t.endAsync();
+    }
+]);

nlw/share/skin/js-test/wikiwyg/t/bz-2476.t.js
@@ -1,29 +1,0 @@
-var t = new Test.Wikiwyg();
-
-t.filters({
-    text: ['wikitext_to_html', 'html_to_wikitext']
-});
-
-t.plan(1);
-
-t.run_is('before', 'after');
-
-/*
-=== {bz: 2476} - Before and After should be identical
---- before
-^Header
-
-one
-two
-three
-four
-
---- after
-^Header
-
-one
-two
-three
-four
-
-*/