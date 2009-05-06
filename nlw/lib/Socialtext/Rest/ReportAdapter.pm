bb028f38ad7ad59e7eb5672f40a9dbeec74ca2fa
Kevin Jones <kevin.jones@socialtext.com>
2009-05-06 12:54:44 -0700

    A reports adapter class for /data
    This allows us to use the gadget api to access
    reports, which means we can start writing proper
    widgets that access reports.
    
    This is a ridiculously small patch!

---
+++ nlw/lib/Socialtext/Rest/ReportAdapter.pm
@@ -0,0 +1,30 @@
+package Socialtext::Rest::ReportAdapter;
+use strict;
+use warnings;
+use base 'Socialtext::Rest::Entity';
+
+sub adapter {
+    my $self = shift;
+    require Socialtext::Reports::REST;
+    return $self->{_adapter} if $self->{_adapter};
+    $Socialtext::Reports::REST::HUB = $self->hub;
+    $self->{_adapter} = Socialtext::Reports::REST->new();
+    $self->{_adapter}->query($self->rest->query);
+    return $self->{_adapter};
+}
+
+sub GET_html {
+    my $self = shift;
+    return $self->adapter->handle_report(
+        $self->name, $self->start, $self->duration, 'html'
+    );
+}
+
+sub GET_json {
+    my $self = shift;
+    return $self->adapter->handle_report(
+        $self->name, $self->start, $self->duration, 'json'
+    );
+}
+
+1;
