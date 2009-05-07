15357f6a5c396c5591e448e6ed21b52565b25d2c
swdevlin <shawn.devlin@socialtext.com>
2009-05-07 13:15:37 -0700

    Add a allows_page_locking workspace config.

--- nlw/etc/socialtext/db/socialtext-56-to-57.sql
+++ nlw/etc/socialtext/db/socialtext-56-to-57.sql
@@ -2,6 +2,9 @@ BEGIN;
 
 CREATE INDEX ix_job_piro_non_null ON job (COALESCE(priority,0));
 
+ALTER TABLE "Workspace"
+    ADD COLUMN allows_page_locking BOOLEAN NOT NULL DEFAULT FALSE;
+
 UPDATE "System"
    SET value = '57'
  WHERE field = 'socialtext-schema-version';
