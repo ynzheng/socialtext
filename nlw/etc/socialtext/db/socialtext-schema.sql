ce2d0e7bd9524fc45de99bbf74704431fab71d14
Audrey Tang <audrey.tang@socialtext.com>
2009-05-07 21:48:35 -0700

    * Update DB schema to 58 in accordance w/ the HOWTO.

--- nlw/etc/socialtext/db/socialtext-schema.sql
+++ nlw/etc/socialtext/db/socialtext-schema.sql
@@ -1455,5 +1455,4 @@ ALTER TABLE ONLY workspace_plugin
             REFERENCES "Workspace"(workspace_id) ON DELETE CASCADE;
 
 DELETE FROM "System" WHERE field = 'socialtext-schema-version';
-
 INSERT INTO "System" VALUES ('socialtext-schema-version', '59');
