f1dc37d796d98997b74a9044a520fd3f35b9c6f6
Audrey Tang <audrey.tang@socialtext.com>
2009-05-14 05:06:19 -0700

    * Visual Test for {bz: 2537}.

---
+++ nlw/share/skin/js-test/s3/t/bz-2537.t.js
@@ -0,0 +1,50 @@
+var t = new Test.Visual();
+var name = t.gensym();
+
+t.plan(1);
+
+var content = '^ Fnord\n\n';
+
+content += '.html\n\n';
+content += '<table>\n\n';
+
+for (var i = 0 ; i < 1000 ; i++) {
+    content += '<tr><td><img src="/data/wafl/' + i + '.png" /></td></tr>\n';
+}
+
+content += '</table>\n\n';
+content += '.html\n\n';
+
+t.runAsync([
+    function() {
+        t.put_page({
+            workspace: 'admin',
+            page_name: name,
+            content: content,
+            tags: ['template'],
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe(
+	    '/admin/?action=display;template=' + name
+	    + ';page_type=wiki;page_name=Untitled%20Page#edit'
+	    , t.nextStep()
+	)
+    },
+
+    t.doRichtextEdit(),
+
+    function() {
+	t.$('#st-newpage-pagename-edit').val(t.gensym());
+	t.callNextStep();
+    },
+
+    t.doSavePage(),
+
+    function() { 
+        t.is( t.$('h1#fnord').length, 1, "Templates with large number of images can be created" );
+        t.endAsync();
+    }
+]);
