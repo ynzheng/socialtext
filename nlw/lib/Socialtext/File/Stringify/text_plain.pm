19c868ffafd61c1e410959fc56a3164c02dc6d9c
Luke Closs <gravatar@5thplane.com>
2009-05-15 11:21:22 -0700

    handle binary data in attachments

--- nlw/lib/Socialtext/File/Stringify/text_plain.pm
+++ nlw/lib/Socialtext/File/Stringify/text_plain.pm
@@ -8,8 +8,14 @@ use Socialtext::l10n qw(system_locale);
 
 sub to_string {
     my ( $class, $filename ) = @_;
-    my $encoding = Socialtext::File::get_guess_encoding(system_locale(), $filename);
-    my $text = scalar Socialtext::File::get_contents_based_on_encoding($filename, $encoding);
+    my $text = '';
+    eval {
+        my $encoding = Socialtext::File::get_guess_encoding(system_locale(), $filename);
+        $text = scalar Socialtext::File::get_contents_based_on_encoding($filename, $encoding);
+    };
+    if ($@) {
+        $text = Socialtext::File::get_contents($filename);
+    }
     return $text;
 }
 
