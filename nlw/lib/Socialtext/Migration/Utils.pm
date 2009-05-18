38cde038b63f85125d2a4c66ed26d1be7785cf4c
Brandon Noard <brandon.noard@socialtext.com>
2009-05-18 12:23:13 -0700

    Rework the migration a little bit.
    I really don't like the idea of updating the schema in a 'check' script,
    so let's assume that if the schema isn't at the correct version, we
    need to run the migration. Then we can let the script that is supposed
    to change data update the schema.
    
    This required adding a 'socialtext_schema_version' sub in
    ST::Migration::Utils.

--- nlw/lib/Socialtext/Migration/Utils.pm
+++ nlw/lib/Socialtext/Migration/Utils.pm
@@ -4,14 +4,18 @@ use strict;
 use warnings;
 use Socialtext::Schema;
 use base 'Exporter'; 
-our @EXPORT_OK = qw/ensure_socialtext_schema/;
+our @EXPORT_OK = qw/socialtext_schema_version ensure_socialtext_schema/;
+
+sub socialtext_schema_version {
+    my $schema = Socialtext::Schema->new;
+    return $schema->current_version;
+}
 
 sub ensure_socialtext_schema {
     my $max_version = shift || die 'A maximum version number is mandatory';
 
     my $schema = Socialtext::Schema->new;
-    my $current_version = $schema->current_version;
-    return if $current_version >= $max_version;
+    return if $schema->current_version >= $max_version;
 
     print "Ensuring socialtext schema is at version $max_version\n";
     $schema->sync( to_version => $max_version );
