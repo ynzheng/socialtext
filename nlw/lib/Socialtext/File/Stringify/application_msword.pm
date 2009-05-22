89babfaa177a7d3f1cb64d681c9b682d6c8b0fb8
Luke Closs <gravatar@5thplane.com>
2009-05-22 15:30:42 -0700

    Use a timeout when calling wvWord - {bz: 643}

--- nlw/lib/Socialtext/File/Stringify/application_msword.pm
+++ nlw/lib/Socialtext/File/Stringify/application_msword.pm
@@ -6,6 +6,8 @@ use warnings;
 use File::Temp;
 use Socialtext::File;
 use Socialtext::File::Stringify::Default;
+use Socialtext::System qw/backtick/;
+use Socialtext::Log qw/st_log/;
 
 sub to_string {
     my ( $class, $filename ) = @_;
@@ -16,13 +18,14 @@ sub to_string {
     );
 
     # If 'wvText' fails, fall back on the "any' mode.
-    my $text =
-        ( system 'wvText', $filename, $temp_filename ) == 0
-        ? Socialtext::File::get_contents_utf8($temp_filename)
-        : Socialtext::File::Stringify::Default->to_string($filename);
-
-    unlink $temp_filename;
-    return $text;
+    my $ignored = backtick('wvText', $filename, $temp_filename);
+    if (my $err = $@) {
+        st_log->warning("Failed to index $filename: $err");
+        return Socialtext::File::Stringify::Default->to_string($filename);
+    }
+    else {
+        return Socialtext::File::get_contents_utf8($temp_filename)
+    }
 }
 
 1;
