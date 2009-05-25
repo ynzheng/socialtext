25840f6debd5ca260e6ce7143cea3ad02faecd18
Audrey Tang <audrey.tang@socialtext.com>
2009-05-24 20:00:49 -0700

    * Adjust {bz: 1620}'s Visual Test to reflect reality.

--- nlw/share/skin/js-test/s3/t/bz-1620.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1620.t.js
@@ -6,15 +6,16 @@ t.plan(1);
 
 t.runAsync([
     function() {
-        t.open_iframe("/?profile/2", t.nextStep());
+        t.open_iframe("/?profile", t.nextStep());
     },
             
     function() { 
-        var rss_link = t.$('iframe.updates').get(0).contentWindow.document.getElementById('rss_link');
+        var rss_link = t.$('iframe[src*=local:people:profile_activities]').get(0).contentWindow.document.getElementById('rss_link');
+
         t.is(
             rss_link.getAttribute('target'),
-            '_blank',
-            "[RSS Feed] link opens in a new window"
+            '_top',
+            "[RSS Feed] link opens in parent window, not a child window"
         );
 
         t.endAsync();
