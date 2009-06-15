77a4eb2e97ffb472559d93be541dfcf94198dc02
Stash <jeremy.stashewsky@socialtext.com>
2009-06-15 15:57:14 -0700

    refactor job adding code for reuse

--- nlw/lib/Socialtext/Migration/Utils.pm
+++ nlw/lib/Socialtext/Migration/Utils.pm
@@ -3,8 +3,11 @@ package Socialtext::Migration::Utils;
 use strict;
 use warnings;
 use Socialtext::Schema;
+use Socialtext::JobCreator;
+use Socialtext::Workspace;
 use base 'Exporter'; 
-our @EXPORT_OK = qw/socialtext_schema_version ensure_socialtext_schema/;
+our @EXPORT_OK = qw/socialtext_schema_version ensure_socialtext_schema
+                    create_job_for_each_workspace/;
 
 sub socialtext_schema_version {
     my $schema = Socialtext::Schema->new;
@@ -19,6 +22,26 @@ sub ensure_socialtext_schema {
 
     print "Ensuring socialtext schema is at version $max_version\n";
     $schema->sync( to_version => $max_version );
+}
+
+sub create_job_for_each_workspace {
+    my $class = shift || die 'A job class is mandatory';
+    my $job_class = 'Socialtext::Job::Upgrade::' . $class;
+
+    my $all = Socialtext::Workspace->All;
+    my $job_count = 0;
+    while (my $ws = $all->next) {
+        Socialtext::JobCreator->insert( $job_class,
+            { workspace_id => $ws->workspace_id },
+        );
+        $job_count++;
+    }
+
+    # And add one more job to clean up global prefs
+    Socialtext::JobCreator->insert( $job_class, { workspace_id => 0 } );
+    $job_count++;
+
+    print "Inserted $job_count $job_class jobs\n";
 }
 
 1;
