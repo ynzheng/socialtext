ba73dae4fb940de25bd84da10d4dec6eff439c77
Kevin Jones <kevin.jones@socialtext.com>
2009-06-22 14:51:22 -0700

    Fix {bz: 2720} - Weeeeirrrd....

--- nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wikitext.js
+++ nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg/Wikitext.js
@@ -2078,7 +2078,7 @@ proto.format_a = function(elem) {
                  .replace(/^\s+/, '') 
                  .replace(/\s+$/, '');
 
-    var href = elem.href;
+    var href = elem.getAttribute('href');
 
     if (! href) href = ''; // Necessary for <a name="xyz"></a>'s
     var link = this.make_wikitext_link(label, href, elem);
@@ -2146,7 +2146,7 @@ proto.is_italic = function(elem) {
 }
 
 proto.elem_is_wiki_link = function (elem) {
-    var href = elem.href || ''
+    var href = elem.getAttribute('href') || ''
     return jQuery(elem).attr('wiki_page')
         || (
             this.href_is_wiki_link(href)
