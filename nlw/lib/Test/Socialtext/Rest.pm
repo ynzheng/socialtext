55cbbdf63d8412264b1a61f35f2dc50a640aedd6
Kevin Jones <kevin.jones@socialtext.com>
2009-06-24 16:50:38 -0700

    Call the directory rest_files not rest_tests

--- nlw/lib/Test/Socialtext/Rest.pm
+++ nlw/lib/Test/Socialtext/Rest.pm
@@ -9,12 +9,12 @@ use Socialtext::System qw(backtick);
 use Socialtext::File qw(get_contents);
 
 (my $nlw = Socialtext::AppConfig->code_base) =~ s{/[^/]+$}{};
-my $tests = "$nlw/t/rest_tests";
+my $files = "$nlw/t/rest_files";
 
 sub GET {
     my $self = shift;
     my $name = $self->name;
-    my $file = "$tests/$name";
+    my $file = "$files/$name";
 
     die "$name doesn't exist!\n" unless -f $file;
     return backtick($file) if -x $file;
