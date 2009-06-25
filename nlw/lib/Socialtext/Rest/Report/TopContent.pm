2c4f0ce460bb9ae93c1f9b76866221a65cba3a40
Brandon Noard <brandon.noard@socialtext.com>
2009-06-25 15:00:50 -0700

    Pass along more data to the widget.

--- nlw/lib/Socialtext/Rest/Report/TopContent.pm
+++ nlw/lib/Socialtext/Rest/Report/TopContent.pm
@@ -36,43 +36,38 @@ override 'GET_json' => sub {
         }, $user,
     ) };
     return $self->error(400, 'Bad request', $@) if $@;
-
-    my $wksp = eval { $report->workspace };
-    return $self->error(400, 'Bad request', $@) if $@;
-    $self->hub->current_workspace($wksp);
-    
     return $self->not_authorized unless $report->is_viewable_by($user);
 
-    my $data;
+    my @pages;
     eval { 
-        $data = $report->_data;
-
+        my $data = $report->_data;
         # Clean up the data
-        my %clean_data;
         for my $row (@$data) {
-            my ($page_id, $count) = @$row;
-            next if $page_id =~ m/^untitled[_\s]page/i;
+            my ($ws_name, $page_id, $count) = @$row;
 
-            my $page;
-            if ($page_id eq '') {
-                $page = $self->hub->pages->new_from_uri($wksp->title);
-            }
-            else {
-                $page = eval { $self->hub->pages->new_from_uri($row->[0]) };
-                if ($@) { warn $@; next }
-            }
-            $clean_data{$page->title} += $row->[1];
+            my $wksp = Socialtext::Workspace->new( name => $ws_name );
+            $page_id ||= $wksp->title;
+
+            $self->hub->current_workspace($wksp);
+
+            my $page  = eval { $self->hub->pages->new_from_uri($page_id) };
+            if ($@) { warn $@; next }
+
+            push @pages, {
+                title          => $page->title,
+                uri            => $page->full_uri,
+                is_spreadsheet => $page->is_spreadsheet,
+                ws_name        => $wksp->name,
+                ws_title       => $wksp->title,
+                ws_uri         => $wksp->uri,
+                count          => $count,
+            };
         }
-        $data = [
-            map { [ $_ => $clean_data{$_} ] }
-                sort { $clean_data{$b} <=> $clean_data{$a} }
-                    keys %clean_data
-        ];
     };
     return $self->error(400, 'Bad request', $@) if $@;
 
     $self->rest->header(-type => 'application/json');
-    return encode_json($data);
+    return encode_json({ pages => \@pages});
 };
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
