0361075f8afc46a987c2ac11ece439f85aa4bdd1
Stash <jeremy.stashewsky@socialtext.com>
2009-07-02 15:01:39 -0700

    Event classes & roles

---
+++ nlw/lib/Socialtext/Events/Event/Person.pm
@@ -0,0 +1,30 @@
+package Socialtext::Events::Event::Person;
+use Moose;
+use MooseX::StrictConstructor;
+use Moose::Util::TypeConstraints;
+use Socialtext::Model::Pages;
+use Socialtext::Workspace;
+use namespace::clean -except => 'meta';
+
+extends 'Socialtext::Events::Event';
+with 'Socialtext::Events::Event::HasPerson';
+
+enum 'PersonEventAction' => qw(
+    edit_save
+    tag_add
+    tag_delete
+    view
+    watch_add
+    watch_delete
+);
+
+has '+action' => (isa => 'PersonEventAction');
+
+after 'build_hash' => sub {
+    my $self = shift;
+    my $h = shift;
+    $h->{event_class} = 'person';
+};
+
+__PACKAGE__->meta->make_immutable;
+1;
