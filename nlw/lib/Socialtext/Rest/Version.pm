c115db19010c678bb7fe896581ddf1247869d8bd
Audrey Tang <audrey.tang@socialtext.com>
2009-07-13 04:36:30 -0700

    * Bump REST API version to 9 for the upcoming /data/events changes.

--- nlw/lib/Socialtext/Rest/Version.pm
+++ nlw/lib/Socialtext/Rest/Version.pm
@@ -13,10 +13,11 @@ use Readonly;
 # 3: iteration-2009-02-27 (release-3.4.1)
 # 4: iteration-2009-03-27 (release-3.5.1): in_reply_to, mentioned_users
 # 5: iteration-2009-04-10 (release-3.5.2): recipient, desktop_* in accounts
-# 6: iteration-2009-04-27 
+# 6: iteration-2009-04-27 (release-3.5.3)
 # 7: iteration-2009-05-22 (release-3.5.5): limit search results
 # 8: iteration-2009-06-05 (release-3.5.6): spreadsheetin'
-Readonly our $API_VERSION => 8;
+# 9: iteration-2009-07-17 (release-3.5.9): /data/events gets new html template and supports account_id
+Readonly our $API_VERSION => 9;
 Readonly our $MTIME       => ( stat(__FILE__) )[9];
 
 sub allowed_methods {'GET, HEAD'}
