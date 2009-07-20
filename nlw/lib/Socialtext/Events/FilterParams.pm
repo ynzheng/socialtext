f220260e5b4d365c1db589569ddb17a01522ebee
Stash <jeremy.stashewsky@socialtext.com>
2009-07-20 12:59:27 -0700

    Support negative filter params

--- nlw/lib/Socialtext/Events/FilterParams.pm
+++ nlw/lib/Socialtext/Events/FilterParams.pm
@@ -14,6 +14,9 @@ use constant AutoFilterParam => 'Socialtext::Events::FilterParams::AutoFilterPar
         is => 'ro', isa => 'Str|CodeRef', 
         predicate => 'has_sql_builder',
     );
+    has 'negative' => (
+        is => 'ro', isa => 'Bool'
+    );
 }
 
 # one thing (defined or not) OR many things (defined)
@@ -24,12 +27,24 @@ sub bunch_of {
 
 sub has_param ($@) {
     my $name = shift;
+
     has $name => (
         is => 'rw',
         traits => [AutoFilterParam],
         predicate => "has_$name",
         clearer => "clear_$name",
         @_,
+        negative => undef,
+    );
+
+    # also register the negative
+    has "not_$name" => (
+        is => 'rw',
+        traits => [AutoFilterParam],
+        predicate => "has_not_$name",
+        clearer => "clear_not_$name",
+        @_,
+        negative => 1,
     );
 }
 
@@ -50,6 +65,13 @@ has_param 'after'  => (isa => 'Str', sql_builder => '_sb_after');
 has 'followed'  => (is => 'rw', isa => 'Bool');
 has 'contributions'  => (is => 'rw', isa => 'Bool');
 
+sub BUILDARGS {
+    my $class = shift;
+    my %p = (@_==1) ? %{+shift} : @_;
+    my %opts = map { (/^(.+)!$/ ? "not_$1" : $_) => $p{$_} } keys %p;
+    return $class->SUPER::BUILDARGS(\%opts);
+}
+
 sub clone {
     my $self = shift;
     return Clone::clone($self);
@@ -57,9 +79,15 @@ sub clone {
 
 sub generate_standard_filter {
     my $self = shift;
+
     my @attrs = grep { $_->does(AutoFilterParam) }
         $self->meta->get_all_attributes;
-    return $self->generate_filter(@attrs);
+
+    my @filter;
+    for my $attr (@attrs) {
+        push @filter, $self->_filter_for_attr($attr);
+    }
+    return \@filter;
 }
 
 sub generate_filter {
@@ -68,25 +96,40 @@ sub generate_filter {
 
     my @filter;
     for my $field (@_) {
-        my $attr = ref($field)
-            ? $field
-            : $meta->find_attribute_by_name($field);
+        my $attr = $meta->find_attribute_by_name($field);
         croak "unknown param '$field'" unless $attr;
-        croak "param '$field' must be handled manually" 
-            unless ($attr->does(AutoFilterParam));
+        push @filter, $self->_filter_for_attr($attr);
 
-        if ($attr->has_sql_builder) {
-            my $sb = $attr->sql_builder;
-            push @filter, $self->$sb($attr);
-        }
-        elsif ($attr->has_value($self)) {
-            my $val = $attr->get_value($self);
-            $val = {-in => $val} if ('ARRAY' eq ref($val));
-            push @filter, $attr->name => $val;
-        }
+        my $dual_field = "not_$field";
+        my $dual_attr = $meta->find_attribute_by_name($dual_field);
+        push @filter, $self->_filter_for_attr($dual_attr) if $dual_attr;
     }
 
     return \@filter;
+}
+
+sub _filter_for_attr {
+    my $self = shift;
+    my $attr = shift;
+
+    croak "param '".$attr->name."' must be handled manually" 
+        unless ($attr->does(AutoFilterParam));
+
+    if ($attr->has_sql_builder) {
+        my $sb = $attr->sql_builder;
+        return  $self->$sb($attr);
+    }
+    return unless $attr->has_value($self);
+
+    my $column = $attr->name;
+    $column =~ s/^not_// if $attr->negative;
+
+    my $val = $attr->get_value($self);
+    my $op = ('ARRAY' eq ref($val))
+        ? ($attr->negative ? '-not_in' : '-in')
+        : ($attr->negative ? '<>'       : '=' );
+        
+    return $column => {$op => $val};
 }
 
 sub _sb_before {
@@ -105,10 +148,7 @@ sub _before_after {
     my $attr = shift;
     my $val = $attr->get_value($self);
     return unless $val;
-    return 'at' => \[
-        "$op ?::timestamptz",
-        $val
-    ];
+    return 'at' => \["$op ?::timestamptz", $val];
 }
 
 __PACKAGE__->meta->make_immutable;
